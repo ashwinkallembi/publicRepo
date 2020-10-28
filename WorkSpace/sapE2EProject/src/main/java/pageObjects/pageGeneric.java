ab5a64c97fe65d4a3650069ae0d4dc84e7f8057a
ashwinkallembi <ashwin.kallembi@gmail.com>
2020-10-29 04:25:11 +0530

    checkin

---
+++ WorkSpace/sapE2EProject/src/main/java/pageObjects/pageGeneric.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import resources.base;
+
+public abstract class pageGeneric {
+	public WebDriver driver;
+
+}
